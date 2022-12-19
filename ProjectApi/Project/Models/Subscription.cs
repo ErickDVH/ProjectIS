063177ac202f4631824f9097ef6d552eb5e204d3
Erick <2203870@my.ipleiria.pt>
2022-12-19 22:59:08 +0000

    models and database

---
+++ ProjectApi/Project/Models/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Creation_dt { get; set; }
+        public int Parent { get; set; }
+        public string Event { get; set; }
+        public string EndPoint { get; set; }
+    }
+}
